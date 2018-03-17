fa89e1b05363a29f9be67f267f936ed769b93fb7
Tim D'hoe <timd93@gmail.com>
2018-03-17 18:17:22 +0100

    Update ubiflux_basic.sh

--- ubiflux_basic.sh
+++ ubiflux_basic.sh
@@ -3,6 +3,7 @@
 #Init if not yet started
 #TODO
 echo "Querying Device..."
+printf \n
 
 #Get Basic Info
 INLETFLOW="$(ebusctl read -f InletFlow)"
@@ -19,12 +20,17 @@ FILTERSTATUS="$(ebusctl read -f FilterStatus)"
 BYPASSOPERATION="$(ebusctl read -f BypassStatus)"
 FROSTSTATUS="$(ebusctl read -f FrostStatus)"
 
-echo "Inside Temperature $INTEMP °C"
-echo "Outside Temperature $OUTTEMP °C"
+echo "###### AIR FLOW INFO ######"
 echo "Anti-frost status is: $FROSTSTATUS, bypass valve status is: $BYPASSOPERATION"
 echo "Fan speed mode is set to $FANSPEEDMODE, with a flow rate of $FANSPEEDM3 m³/h"
-echo "The inlet fan is running at $INLETFANRPM RPM"
-echo "The outlet fan is running at $OUTLETFANRPM RPM"
 echo "Inlet Flow is $INLETFLOW m³/h, with a pressure of $INLETPRESS Pa"
 echo "Outlet Flow is $OUTLETFLOW m³/h, with a pressure of $OUTLETPRES Pa"
+printf \n
+echo "###### TEMPERATURES ######"
+echo "Inside Temperature: $INTEMP °C"
+echo "Outside Temperature: $OUTTEMP °C"
+printf \n
+echo "###### SYSTEM STATUS ######"
+echo "The inlet fan is running at $INLETFANRPM RPM"
+echo "The outlet fan is running at $OUTLETFANRPM RPM"
 echo "The filter is $FILTERSTATUS"
