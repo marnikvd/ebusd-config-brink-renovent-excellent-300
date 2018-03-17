ae49760d7c9c2832242762ae84babbe9a2079fba
Tim D'hoe <timd93@gmail.com>
2018-03-17 17:53:11 +0100

    Update ubiflux_basic.sh

--- ubiflux_basic.sh
+++ ubiflux_basic.sh
@@ -1,8 +1,48 @@
 #!/bin/bash
 
 #Init if not yet started
-//TODO
+#TODO
 
 #Get Basic Info
-echo "SoftwareVersion: "
-ebusctl read -f SoftwareVersion
+INLETFLOW="$(ebusctl read -f InletFlow)"
+INLETPRESS="$(ebusctl read -f PressureInlet)"
+OUTLETFLOW="$(ebusctl read -f ExhaustFlow)"
+OUTLETPRES="$(ebusctl read -f PressureExhaust)"
+FANSPEEDMODE="$(ebusctl read -f FanSpeed)"
+FANSPEEDM3="$(ebusctl read -f SettingInletFlow)"
+OUTTEMP="$(ebusctl read -f OutsideTemperature)"
+INTEMP="$(ebusctl read -f InsideTemperature)"
+INLETFANRPM="$(ebusctl read -f InletFanSpeed)"
+OUTLETFANRPM="$(ebusctl read -f ExhaustFanSpeed)"
+FILTERSTATUS="$(ebusctl read -f FilterStatus)"
+BYPASSOPERATION="$(ebusctl read -f BypassStatus)"
+FROSTSTATUS="$(ebusctl read -f FrostStatus)"
+
+echo $INLETFLOW
+echo $INLETPRESS
+echo $OUTLETFLOW
+echo $OUTLETPRES
+echo $FANSPEEDMODE
+echo $FANSPEEDM3
+echo $OUTTEMP
+echo $INTEMP
+echo $INLETFANRPM
+echo $FILTERSTATUS
+echo $BYPASSOPERATION
+echo $FROSTSTATUS
+
+
+
+echo -n "SoftwareVersion: "
+
+
+echo -n "Current Fan Speed: "
+
+
+
+#echo -n "Inlet Flow: ";ebusctl read -f InletFlow; echo -n " with system pressure of ";ebusctl read -f PressureInlet; echo -n " Pa"
+
+
+#echo -n "Exhaust Flow (m³):"
+#ebusctl read -f ExhaustFlow
+#echo
